b926f0d39657afd7fafdcb98adecb630683c3e46
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2022-11-19 19:19:43 +0000

    added files and code

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
-window.addEventListener('load', function() {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    canvas.width = 500;
-    canvas.height = 500
+import { Player } from "./player.js";
 
-    class Game{
-        constructor(width, height){
-            this.width = width;
-            this.height = height;
-        }
-        update(){
+window.addEventListener("load", function () {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 500;
+  canvas.height = 500;
 
-        }
-        draw(){
-
-        }
+  class Game {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.player = new Player(this);
     }
-})
+    update() {
+        this.player.update();
+    }
+    draw(context){
+      this.player.draw(context);
+    }
+  }
+  const game = new Game(canvas.width, canvas.height);
+  console.log(game);
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    game.update();
+    game.draw(ctx);
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+});
