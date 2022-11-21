2e9565850f1c6684c4c59941203b792f87641321
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2022-11-21 05:01:58 +0000

    particles.js file addded

---
+++ particles.js
@@ -0,0 +1,24 @@
+class Particles {
+    constructor(game) {
+        this.game = game;
+        this.markedForDeletion = false;
+    }
+    update() {
+        this.x -= this.speedX + this.game.speed;
+        this.y -= this.speedY;
+        this.size *= 0.95;
+        if (this.size < 0.5) this.markedForDeletion = true;
+    }
+}
+
+export class Dust extends Particles {
+    
+}
+
+export class Splash extends Particles {
+
+}
+
+export class Fire extends Particles {
+
+}
